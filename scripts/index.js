5523a5de611879af107272d5985a1d04d7d84093
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-24 23:46:20 -0300

    Expresion de Funcion Inmediatamente Invocada

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
 "use strict";
-
 import { getBoard } from "./views/chessboard.js";
 import { getPieces } from "./views/chesspieces.js";
 import { EventListeners } from "./controllers/events.js";
 import { selectItem } from "./controllers/behaviors.js";
-
-getPieces();
-getBoard();
-selectItem();
-EventListeners();
+// Punto de Entrada Principal
+( function () {
+    getPieces();
+    getBoard();
+    selectItem();
+    EventListeners();
+} )()
