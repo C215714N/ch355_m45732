ee2f19e6ae8c8f63088ad24d6394b7ff382984a6
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-10-05 01:44:51 -0300

    Modelo Declaraciones y Selectores

---
+++ scripts/models/global.js
@@ -0,0 +1,11 @@
+// Variables Globales
+export const 
+    d = document,
+    w = window,
+    store = w.localStorage,
+    idb = w.indexedDB,
+    left = 'a',
+    up = 'w',
+    right = 'd',
+    down = 's',
+    className = 'active';
--- scripts/models/selectors.js
+++ scripts/models/selectors.js
@@ -1,9 +1,9 @@
-// Variables Globales
-export const d = document,
-    className = 'active',
+import { d } from './global.js';
+export const
     box = d.querySelector('#app .box'),
     table = d.querySelector('#app .table'),
     btn = d.querySelector('.btn.open-menu'),
     menu = d.querySelector('#menu'),
+    item = menu.querySelectorAll('li'),
     start = d.querySelector('.start-game'),
     control = d.querySelectorAll('.ctrl');
